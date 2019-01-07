4d48b98de2778921d94ff6176e0fbcf4cee49c8b
MacroUniverse <270174408@qq.com>
2019-01-06 22:22:14 -0600

    use "test_all()" in "test_all.h" to include all the tests
    now "main.cpp" is much cleaner

--- main.cpp
+++ main.cpp
@@ -1,64 +1,8 @@
 // comprehensive test of SLISC
 
-#include "test/test_time.h"
-#include "test/test_slisc.h"
-#include "test/test_arithmatic.h"
-#include "test/test_interp1.h"
-#include "test/test_fft.h"
-#include "test/test_random.h"
-#include "test/test_eigen_basics.h"
-#include "test/test_eigen_linsolve.h"
-#include "test/test_eigen_fft.h"
-#include "test/test_disp.h"
-#include "test/test_print.h"
-#include "test/test_coulomb.h"
-//#include "test/test_mparith.h"
-
-using std::cout; using std::endl; //using std::conj;
-void test()
-{
-	// test column major matrix
-	using namespace slisc;
-	CmatDoub a(2,3);
-	a(0) = 0; a(1) = 1; a(2) = 2; a(3) = 3; a(4) = 4; a(5) = 5;
-	cout << a(0, 0) << a(0, 1) << a(0, 2) << endl;
-	cout << a(1, 0) << a(1, 1) << a(1, 2) << endl;
-	exit(EXIT_SUCCESS);
-}
+#include "test/test_all.h"
 
 int main()
 {
-	// temporary test
-	test();
-
-	// systematic tests
-	cout << "test_time()" << endl;
-	test_time();
-	cout << "test_slisc()" << endl;
-	test_slisc();
-	cout << "test_self_op()" << endl;
-	test_self_op();
-	cout << "test_plus_minus_times_devide()" << endl;
-	test_plus_minus_times_devide();
-	cout << "test_interp1()" << endl;
-	test_interp1();
-	cout << "test_fft()" << endl;
-	test_fft();
-	cout << "test_rand()" << endl;
-	test_random();
-	cout << "test_eigen_basics()" << endl;
-	test_eigen_basics();
-	cout << "test_eigen_linsolve()" << endl;
-	test_eigen_linsolve();
-	cout << "test_eigen_fft()" << endl;
-	test_eigen_fft();
-	cout << "test_disp()" << endl;
-	test_disp();
-	cout << "test_print()" << endl;
-	test_print();
-	cout << "test_coulomb()" << endl;
-	test_coulomb();
-	//cout << "test_mparith()" << endl;
-	//test_mparith();
-	cout << "done testing!" << endl;
+	test_all();
 }
---
+++ test/test_all.h
@@ -0,0 +1,51 @@
+// do all available tests of SLISC
+// TODO: test mparith.h not finished
+
+#include "test_time.h"
+#include "test_slisc.h"
+#include "test_arithmatic.h"
+#include "test_interp1.h"
+#include "test_fft.h"
+#include "test_random.h"
+#include "test_eigen_basics.h"
+#include "test_eigen_linsolve.h"
+#include "test_eigen_fft.h"
+#include "test_disp.h"
+#include "test_print.h"
+#include "test_coulomb.h"
+// #include "test/test_mparith.h"
+
+inline void test_all()
+{
+	using std::cout; using std::endl;
+
+	cout << "test_time()" << endl;
+	test_time();
+	cout << "test_slisc()" << endl;
+	test_slisc();
+	cout << "test_self_op()" << endl;
+	test_self_op();
+	cout << "test_plus_minus_times_devide()" << endl;
+	test_plus_minus_times_devide();
+	cout << "test_interp1()" << endl;
+	test_interp1();
+	cout << "test_fft()" << endl;
+	test_fft();
+	cout << "test_rand()" << endl;
+	test_random();
+	cout << "test_eigen_basics()" << endl;
+	test_eigen_basics();
+	cout << "test_eigen_linsolve()" << endl;
+	test_eigen_linsolve();
+	cout << "test_eigen_fft()" << endl;
+	test_eigen_fft();
+	cout << "test_disp()" << endl;
+	test_disp();
+	cout << "test_print()" << endl;
+	test_print();
+	cout << "test_coulomb()" << endl;
+	test_coulomb();
+	//cout << "test_mparith()" << endl;
+	//test_mparith();
+	cout << "done testing!" << endl;
+}
