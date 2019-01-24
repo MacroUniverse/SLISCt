e6543a761a90777cf1e8598dff597383fa7c2403
MacroUniverse <270174408@qq.com>
2019-01-23 23:45:01 -0600

    working on "input.h"

---
+++ SLISC/input.h
@@ -0,0 +1,196 @@
+// auto save input history to file and repeat input when needed
+
+#pragma once
+#include "slisc.h"
+#include "time.h"
+
+namespace slisc {
+
+class Input
+{
+private:
+	// file IO status
+	// 0: no file IO
+	// 1: reading from file
+	// 2: writing to file
+	enum class Stat: Char { NO_IO, WRITE, READ };
+	Stat m_status;
+	std::string m_fname;
+	std::ofstream m_fout;
+	std::ifstream m_fin;
+
+public:
+
+	// default: no file IO
+	Input() : m_status(Stat::NO_IO)
+	{}
+
+	// create new output file
+	// overwrite if exists
+	void newfile(const std::string & fname)
+	{
+		if (m_status == Stat::READ) {
+			m_fin.close();
+		}
+		else if (m_status == Stat::WRITE) {
+			m_fout.close();
+		}
+		m_fname = fname;
+		m_fout.open(fname);
+		m_status = Stat::WRITE;
+	}
+
+	// open existing file for read
+	// create new if not exist
+	// will continue writing if more input required
+	void openfile(const std::string & fname)
+	{
+		if (m_status == Stat::READ) {
+			m_fin.close();
+		}
+		else if (m_status == Stat::WRITE) {
+			m_fout.close();
+		}
+		m_fname = fname;
+		m_fin.open(fname);
+		m_status = Stat::READ;
+	}
+
+
+	void Bool_cin(Bool_O out, const std::string &prompt) {
+		using namespace std;
+		string str;
+		Int i;
+		for (i = 0; i < 10000; ++i) {
+			cout << prompt << " (y/n) ";
+			getline(cin, str);
+			if (str.size() == 1) {
+				if (str[0] == 'y') {
+					out = true;
+					return;
+				}
+				else if (str[0] == 'n') {
+					out = false;
+					return;
+				}
+			}
+			// failed
+			warning("illegal input, try again!");
+			pause(1);
+		}
+	}
+
+	// return 0 if successful
+	// return -1 reached empty line, m_fin position unchanged
+	Int Bool_fin(Bool_O out, const std::string &prompt) {
+		using namespace std;
+		string str;
+		Int i;
+		for (i = 0; i < 10000; ++i) {
+			streampos old_pos = m_fin.tellg();
+			getline(m_fin, str);
+			if (str.size() == 1) {
+				if (str[0] == 'y') {
+					cout << prompt << " (y/n) " << 'y' << endl;
+					out = true;
+					return 0;
+				}
+				else if (str[0] == 'n') {
+					cout << prompt << " (y/n) " << 'n' << endl;
+					out = false;
+					return 0;
+				}
+			}
+			// failed
+			out = false;
+			return -1;
+		}
+	}
+
+	void Bool_fout(Bool_O out, const std::string &prompt) {
+		Bool_cin(out, prompt);
+		if (out)
+			m_fout << 'y' << std::endl;
+		else
+			m_fout << 'n' << std::endl;
+	}
+
+	// input a bool
+	// " (y/n) " will be appended to prompt
+	Bool Input::Bool(const std::string &prompt) {
+		slisc::Bool out;
+		if (m_status == Stat::NO_IO) {
+			// no IO
+			Bool_cin(out, prompt);
+		}
+		else if (m_status == Stat::WRITE) {
+			// write
+			Bool_fout(out, prompt);
+		}
+		else if (m_status == Stat::READ) {
+			// read
+			if (Bool_fin(out, prompt) != 0) {
+				m_status = Stat::WRITE;
+				m_fout.open(m_fname, std::ios::app);
+				m_fin.close();
+				Bool_fout(out, prompt);
+				return out;
+			}
+		}
+		return out;
+	}
+
+	template <class T1, class T2>
+	void Int2_cin(T1 &out1, T2 &out2, const std::string &prompt) {
+		using namespace std;
+		string str;
+		stringstream ss;
+		Int i;
+		for (i = 0; i < 10000; ++i) {
+			cout << prompt << " ";
+			getline(cin, str);
+			if (str.size() >= 3) {
+				ss.str(str);
+				ss >> out1;
+				ss >> out2;
+				return;
+			}
+			// failed
+			warning("illegal input, try again!");
+			pause(1);
+		}
+	}
+
+	// return 0 if successful
+	// return -1 if fail
+	template <class T1, class T2>
+	Int Int2_fin(T1 &out1, T2 &out2, const std::string &prompt) {
+		using namespace std;
+		string str;
+		stringstream ss;
+		Int i;
+		for (i = 0; i < 10000; ++i) {
+			getline(m_fin, str);
+			if (str.size() >= 3) {
+				ss.str(str);
+				ss >> out1;
+				ss >> out2;
+				cout << prompt << " " << out1 << " " << out2 << endl;
+				return 0;
+			}
+			// failed
+			warning("illegal input, try again!");
+			pause(1);
+		}
+	}
+
+	// return 0 if successful
+	// return -1 if fail
+	template <class T1, class T2>
+	void Int2_fout(T1 &out1, T2 &out2, const std::string &prompt) {
+		Int2_cin(out1, out2, prompt);
+		m_fout << out1 << " " << out2 << endl;
+	}
+};
+
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 // comprehensive test of SLISC
 
-#include "test/test_all.h"
+// #include "test/test_all.h"
+#include "SLISC/input.h"
+#include "SLISC/disp.h"
+
+using namespace slisc;
+using namespace std;
 
 int main()
 {
-	test_all();
+	// test_all();
+	/*Int i;
+	Input inp;
+	VecInt x;
+	Int N = 3;
+	x.resize(N); x = 3;
+	inp.newfile("test.txt");
+	for (i = 0; i < N; ++i) {
+		x(i) = inp.Bool("input a bool");
+	}
+	disp(x);
+
+	cout << endl << endl;
+
+	N += 2;
+	x.resize(N); x = 3;
+	inp.openfile("test.txt");
+	for (i = 0; i < N; ++i) {
+		x(i) = inp.Bool("input a bool");
+	}
+	disp(x);
+
+	cout << endl << endl;
+
+	x = 3;
+	inp.openfile("test.txt");
+	for (i = 0; i < N; ++i) {
+		x(i) = inp.Bool("input a bool");
+	}
+	disp(x);*/
+
+	Int x, y, i;
+	Input in;
+	in.newfile("test.txt");
+	for (i = 0; i < 3; ++i)
+		in.Int2_fout(x, y, "input 2 integers");
+	
+	TODO: generate error when input floating point number
 }
