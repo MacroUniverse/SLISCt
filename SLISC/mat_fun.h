f66e96f3734170335475cd4c2fb56f6a8b02e8fa
MacroUniverse <270174408@qq.com>
2019-11-13 02:26:26 -0600

    add exp_mat_sym() to mat_fun.h, not tested

--- SLISC/mat_fun.h
+++ SLISC/mat_fun.h
@@ -6,7 +6,7 @@
 
 namespace slisc {
 
-// out = exp(a*s)
+// out = exp(a*s) for symmetric matrix
 void exp_mat_sym(CmatDoub_O out, CmatDoub_I a, Doub_I s)
 {
 #ifdef SLS_CHECK_SHAPE
@@ -14,14 +14,36 @@ void exp_mat_sym(CmatDoub_O out, CmatDoub_I a, Doub_I s)
         SLS_ERR("not a square matrix!");
 #endif
     Long N = a.n1();
-    VecDoub eigVal(N); CmatDoub eigVec(N, N);
+    VecDoub eigVal(N);
+    CmatDoub eigVec(N, N);
     eig_sym(eigVal, eigVec, a);
     eigVal *= s;
     exp(eigVal, eigVal);
     CmatDoub temp(N, N);
     mul(temp, eigVec, diag(eigVal));
     trans(eigVec);
-    mul(out, temp, eigVec);
+    // TODO: using cBLAS multiplication can save the transpose
+    mul_gen(out, temp, eigVec);
+}
+
+void exp_mat_sym(CmatComp_O out, CmatDoub_I a, Comp_I s)
+{
+#ifdef SLS_CHECK_SHAPE
+    if (a.n1() != a.n2() || !shape_cmp(out, a))
+        SLS_ERR("not a square matrix!");
+#endif
+    Long N = a.n1();
+    VecDoub eigVal(N);
+    VecComp eigValComp(N);
+    CmatDoub eigVec(N, N);
+    eig_sym(eigVal, eigVec, a);
+    Times(eigValComp, eigVal, s);
+    exp(eigValComp, eigValComp);
+    CmatComp temp(N, N);
+    mul(temp, eigVec, diag(eigValComp));
+    trans(eigVec);
+    // TODO: using cBLAS multiplication can save the transpose
+    mul_gen(out, temp, eigVec);
 }
 
 // calculate exp(A*s) * v by U * exp(D*s) * Uh * v;
